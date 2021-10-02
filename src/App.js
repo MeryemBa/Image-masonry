ac515a19ace76e61f900bad97278f5540e6793fe
Meryem Barroug <barrougmeryem@gmail.com>
2021-10-03 00:09:50 +0200

    update the readme file and add logic to handle loading and error

--- src/App.js
+++ src/App.js
@@ -5,29 +5,47 @@ import './App.css'
 
 function App() {
 	const [data, setData] = useState([])
+	const [isLoading, setLoading] = useState(false)
 	const query = `https://api.unsplash.com/photos/random/?count=30&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`
 
 	useEffect(() => {
+		setLoading(true)
 		axios
 			.get(query)
 			.then((res) => {
 				setData(res.data)
+				setLoading(false)
 			})
-			.catch((e) => console.log(e.message))
+			.catch((e) => {
+				setLoading(false)
+				console.log(e.message)
+			})
 	}, [query])
 
 	return (
 		<div className="App">
-			{data.map(({ id, width, height, alt_description, urls: { regular } }) => (
-				<ImageCard
-					key={id}
-					id={id}
-					width={width}
-					height={height}
-					imageURL={regular}
-					alt_description={alt_description}
-				/>
-			))}
+			{!isLoading ? (
+				data.length > 0 ? (
+					data.map(
+						({ id, width, height, alt_description, urls: { regular } }) => (
+							<ImageCard
+								key={id}
+								id={id}
+								width={width}
+								height={height}
+								imageURL={regular}
+								alt_description={alt_description}
+							/>
+						)
+					)
+				) : (
+					<div className="message-container">
+						Oops, something went wrong Please try again.
+					</div>
+				)
+			) : (
+				<div className="message-container">Loading...</div>
+			)}
 		</div>
 	)
 }
